9ea48c863818a0ae2e39f955db1dafb975ee3341
ChrisClevenger <aenguscoen@gmail.com>
2023-06-16 08:58:49 -0500

    Code initial solution

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,18 +2,36 @@
 
 def stock_picker([array]) 
 
+    ## Create empty variables
+    
+    max_profit = 0
+    buy_index = 0
+    sell_index = 0
+
     ## Iterate over each value in [array]
 
-    array.each_with_index do 
+    array.each_with_index do |buy_price, buy_day|
+
         ## For each |value| calculate the difference between the |value| and those at higher indexes
-
-
+        
+        (buy_day + 1...array.lenght).each do |sell_day|
+            sell_price = array[sell_day]
+            profit = sell_price - buy_price 
 
         ## return the indexes of the two values who have the greatest positive variance between them. 
+
+            if profit > max_profit
+                profit = max_profit
+                buy_index = buy_day
+                sell_index = sell_day
+            end
+        end
 
     end 
 
     ## Put the return
+
+    [buy_index, sell_index]
 
 end 
 
