9d90260e0b6a6646f69ab64482f2b6471bc086ac
ChrisClevenger <aenguscoen@gmail.com>
2023-06-16 09:04:06 -0500

    Switch the place of profit and max_profit to fix

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,9 @@
 ## Implement Method w/ input of array 
 
-def stock_picker([array]) 
+def stock_picker(array) 
 
     ## Create empty variables
-    
+
     max_profit = 0
     buy_index = 0
     sell_index = 0
@@ -14,14 +14,14 @@ def stock_picker([array])
 
         ## For each |value| calculate the difference between the |value| and those at higher indexes
         
-        (buy_day + 1...array.lenght).each do |sell_day|
+        (buy_day + 1...array.length).each do |sell_day|
             sell_price = array[sell_day]
             profit = sell_price - buy_price 
 
         ## return the indexes of the two values who have the greatest positive variance between them. 
 
             if profit > max_profit
-                profit = max_profit
+                max_profit = profit
                 buy_index = buy_day
                 sell_index = sell_day
             end
@@ -31,7 +31,9 @@ def stock_picker([array])
 
     ## Put the return
 
-    [buy_index, sell_index]
+    results = [buy_index, sell_index]
+    
+    puts results.inspect
 
 end 
 
