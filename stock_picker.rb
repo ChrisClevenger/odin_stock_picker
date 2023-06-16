19374f6622456f5fa78528b51a69c9d90d668815
ChrisClevenger <aenguscoen@gmail.com>
2023-06-15 21:15:17 -0500

    Build initial framework and comments

---
+++ stock_picker.rb
@@ -0,0 +1,20 @@
+## Implement Method w/ input of array 
+
+def stock_picker([array]) 
+
+    ## Iterate over each value in [array]
+
+    array.each_with_index do 
+        ## For each |value| calculate the difference between the |value| and those at higher indexes
+
+
+
+        ## return the indexes of the two values who have the greatest positive variance between them. 
+
+    end 
+
+    ## Put the return
+
+end 
+
+stock_picker([17,3,6,9,15,8,6,1,10])
